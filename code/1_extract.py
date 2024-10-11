1c8ab2fee27b4cb1ec8c0d681568e515e2a6b23b
Sofia Floresca <sofiafloresca12@gmail.com>
2024-10-10 20:22:02 -0400

    finished part 1

--- code/1_extract.py
+++ code/1_extract.py
@@ -3,5 +3,18 @@ import numpy as np
 import streamlit as st
 import pandaslib as pl
   
-#TODO Write your extraction code here
+states = "https://docs.google.com/spreadsheets/d/14wvnQygIX1eCVo7H5B7a96W1v5VCg6Q9yeRoESF6epw/export?format=csv"
+statenames = pd.read_csv(states)
+states.to_csv('cache/states.csv', index=False)
 
+
+url = "https://docs.google.com/spreadsheets/d/1IPS5dBSGtwYVbjsfbaMCYIWnOuRmJcbequohNxCyGVw/export?resourcekey=&gid=1625408792&format=csv"
+survey = pd.read_csv(url)
+survey["Year"] = survey["Timestamp"].apply(pl.extract_year_mdy)
+survey.to_csv('cache/survey.csv', index=False)
+
+for year in survey["Year"].unique():
+    url = f"https://www.numbeo.com/cost-of-living/rankings.jsp?title={year}&displayColumn=0"
+    col_year = pd.read_html(url)[1]
+    col_year["Year"] = year
+    col_year.to_csv(f"cache/col_{year}.csv", index=False)
