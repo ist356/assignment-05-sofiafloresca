f0f64344d0c5d1dd24a20bade09f64603ad06468
Sofia Floresca <sofiafloresca12@gmail.com>
2024-10-10 20:11:17 -0400

    pandaslib done

--- code/pandaslib.py
+++ code/pandaslib.py
@@ -4,13 +4,19 @@ def clean_currency(item: str) -> float:
     '''
     remove anything from the item that prevents it from being converted to a float
     '''    
-    return 
+    newitem = item.replace('$', '')
+    newitem = newitem.replace(',', '')
+    newitem = float(newitem)
+    return newitem
 
 def extract_year_mdy(timestamp):
     '''
     use the datatime.strptime to parse the date and then extract the year
     '''
-    return 
+
+    date = datetime.strptime(timestamp, '%m/%d/%Y %H:%M:%S')
+    year = date.year
+    return year
 
 def clean_country_usa(item: str) ->str:
     '''
@@ -20,7 +26,9 @@ def clean_country_usa(item: str) ->str:
     possibilities = [
         'united states of america', 'usa', 'us', 'united states', 'u.s.'
     ]
-    return
+    for possibility in possibilities:
+        if possibility in item.lower():
+            return 'United States'
 
 
 if __name__=='__main__':
@@ -28,4 +36,7 @@ if __name__=='__main__':
         Add code here if you need to test your functions
         comment out the code below this like before sumbitting
         to improve your code similarity score.""")
+    '''print(clean_currency('$3,000'))
+    print(extract_year_mdy('12/31/2020'))
+    print(clean_country_usa('United States of America'))'''
 
