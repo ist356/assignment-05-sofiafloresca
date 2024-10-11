64c0d3af5c0f6180dced72d378cf40879833c769
Sofia Floresca <sofiafloresca12@gmail.com>
2024-10-10 20:45:48 -0400

    done part 3

--- code/3_load.py
+++ code/3_load.py
@@ -41,4 +41,7 @@ def upload_file(file_name, bucket_name, object_name=None):
 
 if __name__ == '__main__':
     #TODO: Write your load code here (remove pass first)
-    pass
+    file = ['cache/survey_dataset.csv', 'cache/annual_salary_adjusted_by_location_and_age.csv', 'cache/annual_salary_adjusted_by_location_and_education.csv']
+    for file in files:
+        obj = file.replace('cache/', '')
+        upload_file(file, 'ist356mafudge', obj)
